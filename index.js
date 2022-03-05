ffc801d8840acdcfc2ac6c12e676adf496144cfe
RutujaRM <96306705+RutujaRM@users.noreply.github.com>
2022-03-05 14:35:10 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+                function addition()
+                {
+                        var val1 = parseInt(document.getElementById("value1").value);
+                        
+                        var val2 = parseInt(document.getElementById("value2").value);
+                        
+                        var ans = document.getElementById("answer");
+                       
+                        ans.value = val1 + val2;
+                }
+        
+
+               
